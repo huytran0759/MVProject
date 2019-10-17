fa8af4a8bb44ed92aff1d9757d71f1dbe61e8480
Huy Tran <huytran0759@gmail.com>
2019-10-17 10:51:06 -0700

    Add document and text analysis classes

---
+++ src/TextAnalysis.java
@@ -0,0 +1,53 @@
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class TextAnalysis {
+
+    public static void main(String[] args) {
+    }
+
+    private static ArrayList<String> splitIntoSentences(String text) {
+        ArrayList<String> output = new ArrayList<>();
+
+        Locale locale = Locale.US;
+        BreakIterator breakIterator = BreakIterator.getSentenceInstance(locale);
+        breakIterator.setText(text);
+
+        int prevIndex = 0;
+        int boundaryIndex = breakIterator.first();
+        while (boundaryIndex != BreakIterator.DONE) {
+            String sentence = text.substring(prevIndex, boundaryIndex).trim();
+            if (sentence.length() > 0)
+                output.add(sentence);
+            prevIndex = boundaryIndex;
+            boundaryIndex = breakIterator.next();
+        }
+
+        String sentence = text.substring(prevIndex).trim();
+        if (sentence.length() > 0)
+            output.add(sentence);
+
+        return output;
+    }
+
+    private static ArrayList<String> splitIntoWords(String text) {
+        ArrayList<String> words = new ArrayList<>();
+        String[] splitWords = text.split(" ");
+        for (String word : splitWords) {
+            words.add(word);
+        }
+        return words;
+    }
+
+    private static int countOccurences(ArrayList<String> wordList, String target) {
+        int count = 0;
+        for (String word : wordList) {
+            if (word.equals(target)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
